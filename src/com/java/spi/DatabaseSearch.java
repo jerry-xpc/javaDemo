b2a32609e4e3c6b918ac414657429cca3b3f8882
jerryXing <xpc930302@126.com>
2021-06-15 16:56:22 +0800

    Default Changelist

--- src/com/java/spi/DatabaseSearch.java
+++ src/com/java/spi/DatabaseSearch.java
@@ -6,6 +6,7 @@ public class DatabaseSearch implements Search{
     @Override
     public List<String> searchDoc(String keyword) {
         System.out.println("数据搜索:"+keyword);
+        //test Git
         return null;
     }
 }
