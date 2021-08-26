8581bd4044944e8ef1702d43f5ce8f13a2b1da2f
jerryXing <xpc930302@126.com>
2021-08-26 18:02:50 +0800

    移除lib中包 使用maven引用包

---
+++ src/main/java/com/java/spring/bean/Person.java
@@ -0,0 +1,138 @@
+package com.java.spring.bean;
+
+import java.util.*;
+
+public class Person{
+    private int id;
+    private String name;
+    private int age;
+    private String gender;
+
+    private String[] hobbies;
+    private Address address;
+
+    private List list;
+
+    private List<Address> lists;
+    private Set<String> sets;
+    private Map<String,Object> maps;
+    private Properties properties;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String[] getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(String[] hobbies) {
+        this.hobbies = hobbies;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public List<Address> getLists() {
+        return lists;
+    }
+
+    public void setLists(List<Address> lists) {
+        this.lists = lists;
+    }
+
+    public Set<String> getSets() {
+        return sets;
+    }
+
+    public void setSets(Set<String> sets) {
+        this.sets = sets;
+    }
+
+    public Map<String, Object> getMaps() {
+        return maps;
+    }
+
+    public void setMaps(Map<String, Object> maps) {
+        this.maps = maps;
+    }
+
+    public Properties getProperties() {
+        return properties;
+    }
+
+    public void setProperties(Properties properties) {
+        this.properties = properties;
+    }
+
+    public List getList() {
+        return list;
+    }
+
+    public void setList(List list) {
+        this.list = list;
+    }
+
+
+    public Person() {
+        System.out.println("Person被创建");
+    }
+
+    public Person(int id, String name, int age, String gender) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        System.out.println("Person有参对象被创建");
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                ", hobbies=" + Arrays.toString(hobbies) +
+                ", address=" + address +
+                ", list=" + list +
+                ", lists=" + lists +
+                ", sets=" + sets +
+                ", maps=" + maps +
+                ", properties=" + properties +
+                '}';
+    }
+}
--- src/main/java/com/java/spring/test/Test01.java
+++ src/main/java/com/java/spring/test/Test01.java
@@ -1,6 +1,6 @@
 package com.java.spring.test;
 
-import com.java.spring.bean.Persion;
+import com.java.spring.bean.Person;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -8,7 +8,22 @@ public class Test01 {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("ioc.xml");
 
-        Persion person = (Persion) context.getBean("person");
+        Person person = (Person) context.getBean("person");
         System.out.println(person);
+
+        Person person2 = context.getBean("person2", Person.class);
+        System.out.println(person2);
+
+        Person person3 = context.getBean("person3", Person.class);
+        System.out.println(person3);
+
+        Person person4 = context.getBean("person4", Person.class);
+        System.out.println(person4);
+
+        Person person5 = context.getBean("person5", Person.class);
+        System.out.println(person5);
+
+        Person person6 = context.getBean("person6", Person.class);
+        System.out.println(person6);
     }
 }
